f57a91b3126becdd31e12431a347a9b06214f81b
eldarlrd <eldarlrd@pm.me>
2023-11-10 18:08:34 +0400

    init: Docusaurus

---
+++ CONFIG.md
@@ -0,0 +1,16 @@
+### Install
+```
+$ yarn
+```
+### Local Development
+```
+$ yarn start
+```
+### Build
+```
+$ yarn build
+```
+### Deployment
+```
+$ GIT_USER=<Your GitHub username> yarn deploy
+```
---
+++ docusaurus.config.ts
@@ -0,0 +1,65 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import { type Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Documentation | eldarlrd',
+  tagline: "eldarlrd's intro and tech documentation",
+  favicon: 'https://avatars.githubusercontent.com/u/95304986',
+  url: 'https://eldarlrd.github.io',
+  baseUrl: '/eldarlrd/',
+  organizationName: 'eldarlrd',
+  projectName: 'eldarlrd',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebar.ts'
+        },
+        theme: {
+          customCss: 'theme.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    image: 'https://avatars.githubusercontent.com/u/95304986',
+    navbar: {
+      title: 'eldarlrd',
+      logo: {
+        // Stuff
+        src: 'https://avatars.githubusercontent.com/u/95304986',
+      },
+      items: [
+        {to: '/docs/software/web-dev', label: 'Web Dev', position: 'left'},
+        {
+          href: 'https://github.com/eldarlrd/eldarlrd',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      logo: {
+        // Stuff
+        src: 'https://avatars.githubusercontent.com/u/95304986'
+      },
+      style: 'light',
+      copyright: '© 2023 eldarlrd',
+    },
+    prism: {
+      theme: prismThemes.dracula,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
