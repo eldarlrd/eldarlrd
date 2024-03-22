2a81cf640cfa2e5add94072a4191942a63b47dd9
eldarlrd <eldarlrd@pm.me>
2024-03-22 15:34:51 +0400

    build(prod): deployment

--- assets/js/5e95c892.d0938cfe.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkeldarlrd=self.webpackChunkeldarlrd||[]).push([[9647],{3362:(e,r,s)=>{s.r(r),s.d(r,{default:()=>t});s(9474);var a=s(8923),l=s(6293),d=s(2308),u=s(7221),c=s(6471),n=s(3274);function t(e){return(0,n.jsx)(l.e3,{className:(0,a.A)(d.G.wrapper.docsPages),children:(0,n.jsx)(c.A,{children:(0,u.v)(e.route.routes)})})}}}]);
