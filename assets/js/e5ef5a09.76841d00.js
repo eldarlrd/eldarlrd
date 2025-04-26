2f6c09799b3c62b38bb1fe2edd9655441aa37121
eldarlrd <eldarlrd@pm.me>
2025-04-26 10:09:31 +0400

    build(prod): deployment

--- assets/js/e5ef5a09.76841d00.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkeldarlrd=self.webpackChunkeldarlrd||[]).push([[7847],{5344:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/eldarlrd/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"\ud83d\udcad Blog"}}')}}]);
